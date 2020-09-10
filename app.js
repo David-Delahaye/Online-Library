5c5381b5255ddc6f286d9be57ff135d872208931
David Delahaye <delahaye3@hotmail.co.uk>
2020-09-10 21:10:22 +0100

    add pagination v1

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ const searchInput = document.querySelector('#search')
 const searchButton = document.querySelector('#btn')
 const grid = document.querySelector('.grid')
 
-async function retrieve (query){
-const data = await fetch('https://www.googleapis.com/books/v1/volumes?maxResults=20&q=' + query);
-const jsonData = await data.json();
-return jsonData
+async function retrieve (query, start){
+    console.log(start);
+    const data = await fetch(`https://www.googleapis.com/books/v1/volumes?maxResults=20&filter=ebooks&startIndex=${start}&q=${query}`);
+    const jsonData = await data.json();
+    return jsonData
 }
 
 function s(array){
@@ -34,7 +35,6 @@ async function render (item){
         </ul>
         <button class = "learn">Learn More</button>
     </div>
-
     `
 
     //add data to element
@@ -77,26 +77,28 @@ async function detail(id){
     const pageCount = info.querySelector('.pagecount')
     const publisher = info.querySelector('.publisher')
     const saleLink = info.querySelector('.salelink')
-    title.innerHTML = `<b>${item.volumeInfo.title}</b>` 
-    author.innerHTML = item.volumeInfo.authors || 'N/A'
-    date.innerHTML = '<b>Date Published:</b> ' + item.volumeInfo.publishedDate || ''
-    item.volumeInfo.imageLinks ? image.src = item.volumeInfo.imageLinks.thumbnail || 'N/A' : image.src = 'N/A'
-    description.innerHTML = item.volumeInfo.description || ''
-    pageCount.innerHTML = '<b>Page Count: </b> ' + item.volumeInfo.pageCount || ''
-    publisher.innerHTML = '<b>Publisher: </b> ' + item.volumeInfo.publisher || ''
-    saleLink.href = item.saleInfo.buyLink || '#'
 
-    categories.innerHTML = ''
+    item.volumeInfo.title ? title.innerHTML = item.volumeInfo.title : title.innerHTML =  '';
+    item.volumeInfo.authors ? author.innerHTML = item.volumeInfo.authors : author.innerHTML = '';
+    item.volumeInfo.publishedDate? date.innerHTML = '<b>Date Published:</b> ' + item.volumeInfo.publishedDate : date.innerHTML = '';
+    item.volumeInfo.imageLinks ? image.src = item.volumeInfo.imageLinks.thumbnail || 'N/A' : image.src = 'N/A';
+    item.volumeInfo.description ? description.innerHTML = item.volumeInfo.description : description.innerHTML = 'N/A'
+    item.volumeInfo.pageCount ? pageCount.innerHTML = '<b>Page Count: </b> ' + item.volumeInfo.pageCount : pageCount.innerHTML = '';
+    item.volumeInfo.publisher ? publisher.innerHTML = '<b>Publisher: </b> ' + item.volumeInfo.publisher : publisher.innerHTML='';
+    item.saleInfo.buyLink ? saleLink.href = item.saleInfo.buyLink :  saleLink.innerHTML = '';
+    item.saleInfo.saleability === 'FREE' ? saleLink.innerHTML = 'Read Now' : saleLink.innerHTML = 'Buy Now £' + item.saleInfo.retailPrice.amount;
+
+    categories.innerHTML = '';
+    let i = 0;
     if (item.volumeInfo.categories){
-        item.volumeInfo.categories.forEach((cat) => {
+        for (i = 0; i <= item.volumeInfo.categories.length && i <= 5; i++){
             const tag = document.createElement('li');
             categories.appendChild(tag);
-            tag.innerHTML = cat;
-        })
-        }else{
-            categories.innerHTML += `<li>N/A</li>`
+            tag.innerHTML = item.volumeInfo.categories[i]
         }
-
+    }else{
+        categories.innerHTML=''
+    }
     info.classList.remove('hidden')
 }
 
@@ -105,21 +107,23 @@ function createModal (){
     const info = document.querySelector('#info')
     const modal = `
     <div class="container">
-    <div class="close">X</div>
+    <div class="close"><i class="fas fa-times"></i></div>
     <div class="sidebar">
         <img class="image">
         <p class="date"><b>Date Published: </b></p>
         <p class="publisher"><b>Publisher: </b></p>
         <p class="pagecount"><b>Page Count: </b></p>
-        <p><b>Categories:</b></p>
         <ul class = "categories">
+        <p><b>Categories:</b></p>
         </ul>
-        <a class="salelink" target="_blank">Buy Now</a>
     </div>
     <div class="main">
         <h2 class="title"></h2>
         <em class="author"></em>
         <p class="description"></p>
+    </div>
+    <div class="footer">
+        <a class="salelink" target="_blank">Buy Now</a>
     </div>
     </div>
 `
@@ -135,11 +139,24 @@ const close = document.querySelector('.close');
 close.addEventListener('click', function(){info.classList.add('hidden')})
 
 searchButton.addEventListener('click', async function () {
-    let books = await retrieve(searchInput.value);
-    books = books.items
+    let books = await retrieve(searchInput.value, 0);
+    books = books.items;
     grid.innerHTML = '';
     for (book of books){
         await render(book)
+    }
+    if(grid.childNodes){
+        console.log('here');
+        const loader = document.createElement('button');
+        loader.innerHTML = 'load more';
+        loader.addEventListener('click', async function (){
+            let books = await retrieve(searchInput.value, grid.childNodes.length+1)
+            books = books.items;
+            for (book of books){
+                await render(book)
+            }
+        })
+        document.body.appendChild(loader)
     }
 });
 
